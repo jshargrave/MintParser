f71aac014f7b6124dd3b409de8230f1c56d5b5f2
jshargrave <happyguyhere@hotmail.com>
2021-04-09 20:50:59 -0700

    re-wrote some of the logic to standardize json format

--- Mint_Parser.py
+++ Mint_Parser.py
@@ -2,16 +2,17 @@ from datetime import datetime
 import calendar
 import argparse
 import json
+import copy
 import re
 import os
 
 
 def get_args():
-    action_choices = ["CategoriesByPattern", "CategoriesByColumn"]
+    action_choices = ["GroupByPatternFile", "GroupByColumnValue", "GroupBySearchPattern"]
     date_period_choices = ["Daily", "Weekly", "Monthly", "Yearly"]
     add_help = "Pass the -h argument for more information"
     actions_args_dict = {
-        "CategoriesByPattern": [
+        "GroupByPatternFile": [
             "transactions_file",
             "pattern_file",
             "output_file",
@@ -19,26 +20,40 @@ def get_args():
             "date_column",
             "amount_column",
         ],
-        "CategoriesByColumn": [
+        "GroupByColumnValue": [
             "transactions_file",
             "output_file",
             "date_period",
-            "category_column",
+            "categorize_column",
+            "date_column",
+            "amount_column",
+        ],
+        "GroupBySearchPattern": [
+            "transactions_file",
+            "search_pattern",
+            "output_file",
+            "date_period",
             "date_column",
             "amount_column",
         ],
     }
 
-    parser = argparse.ArgumentParser(description='Used to process arguments passed to the Mint_Parser.py.')
+    usage = " Mint_Parser.py [-h]\n" \
+            "                       --action GroupByPatternFile --transactions_file TRANSACTIONS_FILE --pattern_file PATTERN_FILE\n" \
+            "                       --action GroupByColumnValue --transactions_file TRANSACTIONS_FILE --categorize_column CATEGORIZE_COLUMN\n" \
+            "                       --action GroupBySearchPattern --transactions_file TRANSACTIONS_FILE --search_pattern SEARCH_PATTERN\n"
 
-    parser.add_argument('--action', default=["CategoriesByPattern"], nargs='+', choices=action_choices, help='List of actions to perform.  Currently there is only one action that can be passed, which is ParseTransactions.')
-    parser.add_argument('--transactions_file', default="transactions.csv", help='Used to point to the transaction file that was exported from Mint. Default is transactions.csv.')
-    parser.add_argument('--pattern_file', default="category_patterns_default.json", help='JSON file that contains a series of patterns. See category_patterns.json.template for an example. The patterns are regulare expressions.')
-    parser.add_argument('--output_file', default="output.json", help='File to output the results of matching the patterns from the --pattern_file argument.')
-    parser.add_argument('--date_period', default="Monthly", choices=date_period_choices, help='The period of summation to use on the transactions when building the report.')
-    parser.add_argument('--category_column', default="Description", help='Column to group the transactions by. Must match to a column name in --transactions_file')
-    parser.add_argument('--date_column', default="Date", help='Column to group the transactions by. Must match to a column name in --transactions_file')
-    parser.add_argument('--amount_column', default="Amount", help='Column to group the transactions by. Must match to a column name in --transactions_file')
+    parser = argparse.ArgumentParser(description='Used to process arguments passed to the Mint_Parser.py.', usage=usage)
+
+    parser.add_argument('--action', default=["GroupByPatternFile"], nargs='+', choices=action_choices, help='List of actions to perform. Can be one or multiples of the following: \"{}\"'.format("\" \"".join(action_choices)))
+    parser.add_argument('--transactions_file', default="transactions.csv", help='Used to point to the transaction file exported from Mint. Default is transactions.csv.')
+    parser.add_argument('--pattern_file', default="category_patterns_default.json", help='JSON file that contains a series of regular expressions to match patterns in the --transactions_file. See category_patterns_default.json for an example.')
+    parser.add_argument('--search_pattern', help='Regular expression to use for searching through transactions.')
+    parser.add_argument('--output_file', default="output.json", help='File to output the results to.')
+    parser.add_argument('--date_period', default="Monthly", choices=date_period_choices, help='The period of summation to use on the transactions when building the report. Can be one of the following: \"{}\"'.format("\" \"".join(date_period_choices)))
+    parser.add_argument('--categorize_column', default="Category", help='Column to group transactions by. Must match to a column name in --transactions_file')
+    parser.add_argument('--date_column', default="Date", help='Column to extract the amount date from. Must match a column name in --transactions_file')
+    parser.add_argument('--amount_column', default="Amount", help='Column to extract the amount from. Must match a column name in --transactions_file')
 
     args = parser.parse_args()
 
@@ -55,17 +70,22 @@ def get_args():
                 msg = "Error: The argument --{} is required when passing the argument --action {}. {}".format(key, ", ".join(args.action), add_help)
                 raise argparse.ArgumentError(msg)
 
-    # CategoriesByPattern
-    if "CategoriesByPattern" in args.action and not os.path.exists(args.transactions_file):
-        msg = "Error: --transactions_file must point to a valid file when passing the argument --action ParseTransactions. {}".format(add_help)
+    # GroupByPatternFile
+    if "GroupByPatternFile" in args.action and not os.path.exists(args.transactions_file):
+        msg = "Error: --transactions_file must point to a valid file when passing the argument --action GroupByPatternFile. {}".format(add_help)
         raise argparse.ArgumentError(msg)
-    if "CategoriesByPattern" in args.action and not os.path.exists(args.pattern_file):
-        msg = "Error: --pattern_file must point to a valid file when passing the argument --action ParseTransactions. {}".format(add_help)
+    if "GroupByPatternFile" in args.action and not os.path.exists(args.pattern_file):
+        msg = "Error: --pattern_file must point to a valid file when passing the argument --action GroupByPatternFile. {}".format(add_help)
         raise argparse.ArgumentError(msg)
 
-    # CategoriesByColumn
-    if "CategoriesByColumn" in args.action and not os.path.exists(args.transactions_file):
-        msg = "Error: --transactions_file must point to a valid file when passing the argument --action ParseTransactions. {}".format(add_help)
+    # GroupByColumnValue
+    if "GroupByColumnValue" in args.action and not os.path.exists(args.transactions_file):
+        msg = "Error: --transactions_file must point to a valid file when passing the argument --action GroupByColumnValue. {}".format(add_help)
+        raise argparse.ArgumentError(msg)
+
+    # GroupBySearchPattern
+    if "GroupBySearchPattern" in args.action and not os.path.exists(args.transactions_file):
+        msg = "Error: --transactions_file must point to a valid file when passing the argument --action GroupBySearchPattern. {}".format(add_help)
         raise argparse.ArgumentError(msg)
 
     return args
@@ -74,13 +94,20 @@ def get_args():
 def main():
     args = get_args()
     for a in args.action:
-        if a == "CategoriesByPattern":
-            categories_by_pattern(args)
-        elif a == "CategoriesByColumn":
-            categories_by_column(args)
+        if a == "GroupByPatternFile":
+            group_by_pattern_file(args)
+        elif a == "GroupByColumnValue":
+            group_by_column_value(args)
+        elif a == "GroupBySearchPattern":
+            group_by_search_pattern(args)
 
 
-def categories_by_pattern(args):
+def group_by_pattern_file(args):
+    temp_category_dict = {
+        "Total": 0,
+        "Transactions": [],
+        args.date_period: {},
+    }
     category_dict = {}
 
     # Check that transactions file is valid path before opening
@@ -130,19 +157,15 @@ def categories_by_pattern(args):
                 if match:
                     found_match = True
                     if key not in category_dict.keys():
-                        category_dict[key] = {
-                            "Transactions": [line],
-                            "Total": amount_flt,
-                            args.date_period: {},
-                        }
-                    else:
-                        category_dict[key]["Transactions"].append(line)
-                        category_dict[key]["Total"] = category_dict[key]["Total"] + amount_flt
+                        category_dict[key] = copy.deepcopy(temp_category_dict)
 
                     if date_key not in category_dict[key][args.date_period].keys():
                         category_dict[key][args.date_period][date_key] = amount_flt
                     else:
-                        category_dict[key][args.date_period][date_key] = category_dict[key][args.date_period][date_key] + amount_flt
+                        category_dict[key][args.date_period][date_key] += amount_flt
+
+                    category_dict[key]["Transactions"].append(line)
+                    category_dict[key]["Total"] += amount_flt
 
                     # If match was found, then continue to next line
                     break
@@ -152,21 +175,16 @@ def categories_by_pattern(args):
         # Record the transaction if no pattern matched it
         if not found_match:
             key = "NO_MATCH"
-            found_match = True
             if key not in category_dict.keys():
-                category_dict[key] = {
-                    "Transactions": [line],
-                    "Total": amount_flt,
-                    args.date_period: {},
-                }
-            else:
-                category_dict[key]["Transactions"].append(line)
-                category_dict[key]["Total"] = category_dict[key]["Total"] + amount_flt
+                category_dict[key] = copy.deepcopy(temp_category_dict)
 
             if date_key not in category_dict[key][args.date_period].keys():
                 category_dict[key][args.date_period][date_key] = amount_flt
             else:
-                category_dict[key][args.date_period][date_key] = category_dict[key][args.date_period][ date_key] + amount_flt
+                category_dict[key][args.date_period][date_key] += amount_flt
+
+            category_dict[key]["Transactions"].append(line)
+            category_dict[key]["Total"] += amount_flt
         count += 1
 
     # Write dictionary to json file
@@ -174,12 +192,13 @@ def categories_by_pattern(args):
         json.dump(category_dict, file_out, sort_keys=True, indent=4, ensure_ascii=False)
 
 
-def categories_by_column(args):
-    category_dict = {
-        "Transactions": {},
-        "Total": {},
-        args.category_column: {},
+def group_by_column_value(args):
+    temp_category_dict = {
+        "Total": 0,
+        "Transactions": [],
+        args.date_period: {},
     }
+    category_dict = {}
 
     # Check that transactions file is valid path before opening
     if not os.path.exists(args.transactions_file):
@@ -205,28 +224,75 @@ def categories_by_column(args):
         date_key = get_date_key(args, date_str)
 
         # Extract column to group by
-        column_key = get_column_value(args.category_column, line, header_text)
+        column_key = get_column_value(args.categorize_column, line, header_text)
 
         # Extract amount
         amount_flt = float(get_column_value(args.amount_column, line, header_text))
 
-        if date_key not in category_dict["Transactions"]:
-            category_dict["Transactions"][date_key] = [line]
-        else:
-            category_dict["Transactions"][date_key].append(line)
+        if column_key not in category_dict.keys():
+            category_dict[column_key] = copy.deepcopy(temp_category_dict)
 
-        if date_key not in category_dict["Total"]:
-            category_dict["Total"][date_key] = amount_flt
+        if date_key not in category_dict[column_key][args.date_period]:
+            category_dict[column_key][args.date_period][date_key] = amount_flt
         else:
-            category_dict["Total"][date_key] += amount_flt
+            category_dict[column_key][args.date_period][date_key] += amount_flt
 
-        if date_key not in category_dict[args.category_column]:
-            category_dict[args.category_column][date_key] = {}
+        if date_key not in category_dict[column_key][args.date_period]:
+            category_dict[column_key][args.date_period][date_key] = {}
 
-        if column_key not in category_dict[args.category_column][date_key]:
-            category_dict[args.category_column][date_key][column_key] = amount_flt
-        else:
-            category_dict[args.category_column][date_key][column_key] += amount_flt
+        category_dict[column_key]["Transactions"].append(line)
+        category_dict[column_key]["Total"] += amount_flt
+
+        count += 1
+
+    # Write dictionary to json file
+    with open(args.output_file, 'w') as file_out:
+        json.dump(category_dict, file_out, sort_keys=True, indent=4, ensure_ascii=False)
+
+
+def group_by_search_pattern(args):
+    category_dict = {
+        "Total": 0,
+        "Transactions": [],
+        args.date_period: {},
+    }
+
+    # Check that transactions file is valid path before opening
+    if not os.path.exists(args.transactions_file):
+        print("Error: {} not found.".format(args.transactions_file))
+        print("Override by passing path to --transactions_file if needed.")
+        exit(1)
+
+    # Read in all transactions
+    with open(args.transactions_file, 'r') as file_in_transactions:
+        file_lines = file_in_transactions.readlines()
+
+    count = 0
+    header_text = ""
+    for line in file_lines:
+        # Don't parse first line
+        if count == 0:
+            header_text = line
+            count += 1
+            continue
+
+        # Extract date and make date formats match
+        date_str = get_column_value(args.date_column, line, header_text)
+        date_key = get_date_key(args, date_str)
+
+        # Extract amount
+        amount_flt = float(get_column_value(args.amount_column, line, header_text))
+
+        match = re.findall(args.search_pattern, line)
+        if match:
+            if date_key not in category_dict[args.date_period]:
+                category_dict[args.date_period][date_key] = amount_flt
+            else:
+                category_dict[args.date_period][date_key] += amount_flt
+
+            category_dict["Transactions"].append(line)
+            category_dict["Total"] += amount_flt
+        count += 1
 
     # Write dictionary to json file
     with open(args.output_file, 'w') as file_out:
